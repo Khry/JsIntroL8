79fbb2cdad4e9de7cbd6de79f021fbf719e251ab
Khry <khrustyk@gmail.com>
2017-07-12 19:22:09 +0300

    add

---
+++ js/task2.js
@@ -0,0 +1,4 @@
+var euro = parseInt(prompt("Введіть кількість euro",""));
+var doll = parseInt(prompt("Введіть кількість dollars",""));
+
+alert(euro + " euros are equal " + euro * 29.772 + " UAH, " + doll + " dollars are equal " + doll * 25.939 + " UAH, one euro is equal " + 29.772 / 25.939 + " dollars."); 
